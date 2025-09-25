4d870e69220d2ca610c44a091a09295b366dc6e5
humza <humzaqasim1@gmail.com>
2025-09-25 13:36:52 -0500

    Updated formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+
+        String name = myScanner.nextLine();
+
+        System.out.println("Enter your hours worked: ");
+
+        String input = myScanner.nextLine();
+
+        double hoursWorked = Double.parseDouble(input);
+
+        System.out.println("Enter your pay rate: ");
+
+        input = myScanner.nextLine();
+
+        double payRate = Double.parseDouble(input);
+
+        System.out.println(name + "'s gross pay is " + (payRate * hoursWorked));
+    }
 }
